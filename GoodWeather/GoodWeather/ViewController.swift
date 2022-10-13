dc687a4257562f4519cf321d38b9ea0948e5957f
Henrique Alves Batochi <32850493+halvesbatochi@users.noreply.github.com>
2022-10-13 11:10:34 -0300

    Building User Interface for the App

---
+++ GoodWeather/GoodWeather/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  GoodWeather
+//
+//  Created by Henrique Alves Batochi on 13/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var cityNameTextField: UITextField!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var humidityLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
