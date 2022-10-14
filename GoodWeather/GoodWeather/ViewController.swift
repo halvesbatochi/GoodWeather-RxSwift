378b77db34787a1bb890383636b47040274ad0f6
Henrique Alves Batochi <32850493+halvesbatochi@users.noreply.github.com>
2022-10-14 17:21:53 -0300

    Displaying Data Using Binding Observables

--- GoodWeather/GoodWeather/ViewController.swift
+++ GoodWeather/GoodWeather/ViewController.swift
@@ -44,16 +44,17 @@ class ViewController: UIViewController {
         
         let resource = Resource<WeatherResult>(url: url)
         
-        URLRequest.load(resource: resource)
+        let search = URLRequest.load(resource: resource)
             .observeOn(MainScheduler.instance)
             .catchErrorJustReturn(WeatherResult.empty)
-            .subscribe(onNext: { result in
-                
-                
-                let weather = result.main
-                self.displayWeather(weather)
-                
-            }).disposed(by: disposeBag)
+        
+        search.map { "\($0.main.temp) ℉"}
+            .bind(to: self.temperatureLabel.rx.text)
+            .disposed(by: disposeBag)
+        
+        search.map { "\($0.main.humidity) 🌦" }
+            .bind(to: self.humidityLabel.rx.text)
+            .disposed(by: disposeBag)
     }
     
     private func displayWeather(_ weather: Weather?) {
